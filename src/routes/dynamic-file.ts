a1aa3d9abd8513848d235757ee055086fc0ddbbc
Saeid Esma <saeid.esma@gmail.com>
2024-09-24 20:20:02 -0500

    (feat): add route file

---
+++ src/routes/dynamic-file.ts
@@ -0,0 +1,41 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { container } from '../inversify.config';
+import { FigmaController } from '../controllers/figma.controller';
+
+module.exports = async function (server: FastifyInstance) {
+  server.get('/figma/file/:fileId', {
+    schema: {
+      description: 'Fetch data from Figma API using a specific file ID',
+      tags: ['Figma'],
+      params: {
+        type: 'object',
+        properties: {
+          fileId: { type: 'string' },
+        },
+      },
+      response: {
+        200: {
+          description: 'Successful response',
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              content: { type: 'string' },
+              fontSize: { type: 'number' },
+              fontFamily: { type: 'string' },
+              fontWeight: { type: 'number' },
+              lineHeight: { type: 'number' },
+              width: { type: 'number' },
+              height: { type: 'number' },
+            },
+          },
+        },
+      },
+    },
+  }, async (request:FastifyRequest, reply:FastifyReply) => {
+    const { fileId } = request.params as { fileId: string };
+    const figmaController = container.resolve(FigmaController);
+    const result = await figmaController.getDataByFileId(fileId, reply);
+    reply.send(result);
+  });
+}
